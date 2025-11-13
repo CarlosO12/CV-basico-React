c20617c9deed80aa538228c6c030b95924c0779b
Carlos Osorio <carlososoriocastro2103@gmail.com>
2025-11-12 23:58:12 -0500

    feat: componente Perfil con descripción personalizada

---
+++ src/components/Perfil.jsx
@@ -0,0 +1,11 @@
+export default function Perfil() {
+  return (
+    <section>
+      <h3>Perfil</h3>
+      <p>
+        Tecnologo en Analisis y Desarrollo de software con experiencia en desarrollo web y móvil, apasionado por la tecnología, tambien soy un poco perfeccionista con los sistemas que desarrollo.
+      </p>
+      <p>Habilidades blandas: Aprendizaje rapido, Responsabilidad, trabajo en equipo</p>
+    </section>
+  );
+}
