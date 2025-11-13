dd535a3c21c5fbd0b70c5ed9c8600fb921413cce
Carlos Osorio <carlososoriocastro2103@gmail.com>
2025-11-12 23:50:12 -0500

    feat: componente CabeceraCV con datos personales

---
+++ src/components/CabeceraCV.jsx
@@ -0,0 +1,12 @@
+export default function CabeceraCV() {
+  return (
+    <header>
+      <h1>Carlos Andres Osorio</h1>
+      <h2>Desarrollador UI Frontend</h2>
+      <h2>Datos de contacto</h2>
+      <h3>3013553417</h3>
+      <h3>carlososoriocastro2103@gmail.com</h3>
+      <h3>Medellín</h3>
+    </header>
+  );
+}
